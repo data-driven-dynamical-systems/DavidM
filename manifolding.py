b4407b6f0f1906037824891aaa8dbb2ab4fee0a3
davidmiller2022 <49624686+davidmiller2022@users.noreply.github.com>
2019-04-22 03:53:12 -0500

    Add files via upload
    added simplex prediction to manifolding.py.  The class is still called CCM, whoops.  Maybe I should break cross-mapping and predicting into two...

--- manifolding.py
+++ manifolding.py
@@ -3,7 +3,53 @@ class CCM:
         self.data = data
         self.N = len(data[:,0])
         
+    
+    def predict(self, tau, predictee_dim, manifold_dim, num_neighbors=None, plotTime = 200):
+        #time series to consider
+        Y = self.data[:,predictee_dim]
         
+        #create delay coordinate manifold
+        M_Y = self.make_manifold(Y, manifold_dim, tau)
+        
+        # number of manifold points
+        N2 = self.N - manifold_dim*tau
+        
+        #size of simplex
+        if num_neighbors is None:
+            k = manifold_dim+1
+        else:
+            k = num_neighbors
+            
+        half = int(N2/2)
+        Y_pred = np.zeros(self.N)
+        Y_pred[:half]= Y[:half]
+        
+        point = np.zeros(manifold_dim)
+        #given first half of data, try to continue series
+        for i in range(half, self.N):
+            #grab manifold point to predict next
+            #oldest index used
+            timeIndex = i-1-(manifold_dim-1)*tau
+            for j in range(manifold_dim):
+                #left entry is most delayed, manifold_dim*tau, 
+                point[j] = Y_pred[timeIndex]
+                timeIndex += tau
+                
+            #new predicted value using first-half manifold
+            new = self.k_simplex_predict(point, M_Y, k, half)
+            
+            #record new value
+            Y_pred[i]=new
+            
+        #might not be fair to compare same distance out for different stencil lengths, 
+        #since longer stencils use true data for longer
+        plt.plot(range(half,half+plotTime),Y[half:half+plotTime])
+        plt.plot(range(half,half+plotTime),Y_pred[half:half+plotTime])
+        #plt.plot(range(half,half+30*tau),Y[half:half+30*tau])
+        #plt.plot(range(half,half+30*tau),Y_pred[half:half+30*tau])
+        plt.xlabel('$t$')
+        plt.ylabel('$Y$')
+        plt.show()
         
     '''
     perform CCM to 'predict' the time series of the predictee variable 
@@ -38,7 +84,7 @@ class CCM:
         else:
             k = num_neighbors
         
-        #for each manifold point, use first half of data
+        #for each manifold point
         for i in range(0, N2):
             #change last input to max(i,k+1) if want to use all up to self
             #int(N2/2)to use first half of data
@@ -99,7 +145,7 @@ class CCM:
         
     '''
     returns [distance, index] for the k closest points to the passed point
-    on manifold up to time t
+    on the passed manifold up to time t
     '''
     def k_nearest_neighbors(self, point, manifold, k, t):
         #subtract our last point from all previous
@@ -126,9 +172,9 @@ class CCM:
         
     def make_manifold(self, X, dim, tau):
         N = self.N
-        manifold = np.zeros([N-(dim-1)*tau , dim])
+        manifold = np.zeros([N-dim*tau , dim])
         
-        for i in range(0, N-(dim-1)*tau):
+        for i in range(0, N - dim*tau):
             for j in range(0, dim):
                 #upper left is x[0], beside it is x[tau], then x[2tau] 
                 manifold[i,j] = X[i+tau*j]
@@ -137,15 +183,22 @@ class CCM:
         
     
     '''
+run prep.py, dynamical_system.py, Lorenz.py, then:
+    
 lorrie = Lorenz(28,10,8/3)
 sol = lorrie.integrate(0,50,0.01, np.array([1,1,1]))
 lorrie.trajectory_plot()
 manny = CCM(sol.y.T)
+
+manny.predict(20,1,4, plotTime = 300)
+
 manny.cross_map(1,1,0,3)
 
 compare to
 
 manny = CCM(np.random.rand(5000,2))
 manny.cross_map(1,1,0,3)
+
+
     '''
     
